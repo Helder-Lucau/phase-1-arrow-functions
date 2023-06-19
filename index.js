a158336a630e8a8e7351d02eaa16f12c1c961e14
Helder <helderlucau@gmail.com>
2023-06-19 12:39:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// Function expression called divide
 const divide = function(){
     return 2000/100
 };
 
+//Arrow function call square
 const square = (x) => {
     return x**x;
 }
 square(2);
 
+//arrow function called add
 const add = (a,b) => {
     return a+b;
 }
